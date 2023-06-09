68f6327dd69089b65f6dde0e35943ba1d9864452
diezes92 <didierdiekpa@yahoo.fr>
2023-06-08 18:28:30 -0700

    tasks

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    for item in dir(hidden_4):
+        if item[0] != '_' and item[1] != '_':
+            print("{}".format(item))
