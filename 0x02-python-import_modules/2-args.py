24719c5c1cdc438675253142e0be60c213c999e1
diezes92 <didierdiekpa@yahoo.fr>
2023-06-08 17:47:11 -0700

    tasks

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,16 @@
 if __name__ == "__main__":
     import sys
     lgt = len(sys.argv) - 1
+
     if lgt == 0:
         print("{} arguments.".format(lgt))
     elif lgt == 1:
         print("{} argument:".format(lgt))
     else:
         print("{} arguments:".format(lgt))
+
     if lgt == 1:
-        print("{}: {}".format(lgt, sys.argv[1])
+        print("{}: {}".format(lgt, sys.argv[1]))
     elif lgt > 1:
         for i in range(1, lgt + 1, 1):
-            print("{}: {}".format(lgt, sys.argv[i]))
+            print("{}: {}".format(i, sys.argv[i]))
