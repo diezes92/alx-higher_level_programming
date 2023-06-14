0bd16390caf4e8d75b8c121b02a53fe68f78d558
diezes92 <didierdiekpa@yahoo.fr>
2023-06-14 14:59:52 -0700

    tasks

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    t = sorted(a_dictionary)
+    t = list(a_dictionary.keys())
     for i in range(len(t)):
         a_dictionary[t[i]] = a_dictionary[t[i]] * 2
     return(a_dictionary)
