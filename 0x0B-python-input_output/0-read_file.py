60811120b9064c2e68b823ed97cad119d7b07719
diezes92 <didierdiekpa@yahoo.fr>
2023-07-12 14:27:14 -0700

    tasks

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that contains a function that reads from a file """
+
+
+def read_file(filename=""):
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+
+    with open(filename, 'r', encoding="utf-8") as fl:
+        data = fl.read()
+        print(data, end='')
